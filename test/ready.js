69de6d96d9ef6178f0f3521fb6280b036832500e
yize <yize.shc@gmail.com>
2013-08-13 14:34:56 +0800

    fixbug

--- test/ready.js
+++ test/ready.js
@@ -3,6 +3,7 @@ var win = UT.open("http://guang.taobao.com", function () {
         it("跳转到淘宝逛逛页面", function () {
             var result = -1 != location.href.indexOf("http://guang.taobao.com");
             expect(result).toBe(true);
+            location.href = "http://www.taobao.com"
         })
     })
 });
